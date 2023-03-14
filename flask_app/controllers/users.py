e7e6bc865fe81aa8a9924404f3050fd87ea248bf
Ruveyda <albayrakruveyda@gmail.com>
2023-03-14 15:23:08 -0700

    update

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -1,7 +1,7 @@
 from flask_app.models.user import User
 from flask_app import app
 from flask import render_template, request, redirect,session,flash
-from flask_bcrypt import Bcrypt
+from flask-bcrypt import Bcrypt
 bcrypt= Bcrypt(app)
 
 
